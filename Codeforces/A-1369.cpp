38fd8644e92e8c44562ad0baba047b49cd6f8857
Abhijeet Sonkar <abhijeet.iiita.iib@gmail.com>
2020-06-26 20:31:56 +0530

    Added solution to FashionabLee

---
+++ Codeforces/A-1369.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const ll mod = 1e9 + 7;
+
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    if(n%4==0)
+    cout<<"Yes"<<endl;
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+
+}
+
+int main()
+{
+    
+    int t;
+    cin>>t;
+    while(t)
+    {
+        solve();
+        --t;
+    }
+
+    return 0;
+}
