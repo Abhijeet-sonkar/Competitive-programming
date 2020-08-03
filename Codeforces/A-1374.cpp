9c1031b6d2b9aed262ef8bf6c1b69f593edabc90
Abhijeet Sonkar <abhijeet.iiita.iib@gmail.com>
2020-08-03 19:11:05 +0530

    Added solution to "A-1374 Required Remainder"

---
+++ Codeforces/A-1374.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const ll mod = 1e9 + 7;
+
+void init()
+{
+
+}
+
+void solve()
+{
+    int  x,y,n;
+    cin>>x>>y>>n;
+    cout<<x*((n-y)/x)+y<<endl;
+
+}
+
+int main()
+{
+    init();
+    int t;
+    cin>>t;
+    while(t)
+    {
+        solve();
+        --t;
+    }
+
+    return 0;
+}
